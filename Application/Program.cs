ddcec061b9ab781130a72b3df90d7bc4faf964ed
Ilya Bondarik <il.bandaryk@gmail.com>
2021-12-19 03:07:58 +0300

    new file

---
+++ Application/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Views.Main;
+
+namespace Application
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            System.Windows.Forms.Application.EnableVisualStyles();
+            System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
+            System.Windows.Forms.Application.Run(new MainForm());
+        }
+    }
+}
